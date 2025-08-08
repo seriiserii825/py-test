e507a0c3377ab34cd4f0aad7e1d882f8ebf63a87
serii <seriiburduja@gmail.com>
2025-08-08 09:26:23 +0300

    feat: group tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,10 +2,19 @@ import pytest
 from main import Calculator
 
 
-@pytest.mark.parametrize("x, y, res", [
-    (1, 2, 0.5),
-    (2, -4, -0.5),
-    (40, 3, 13.333333333333334),
-])
-def test_divide(x, y, res):
-    assert Calculator().divide(x, y) == res
+class TestCalculator:
+    @pytest.mark.parametrize("x, y, res", [
+        (1, 2, 0.5),
+        (2, -4, -0.5),
+        (40, 3, 13.333333333333334),
+    ])
+    def test_divide(self, x, y, res):
+        assert Calculator().divide(x, y) == res
+
+    @pytest.mark.parametrize("x, y, res", [
+        (1, 2, 2),
+        (2, -4, -8),
+        (40, 3, 120),
+    ])
+    def test_multiply(self, x, y, res):
+        assert Calculator().multiply(x, y) == res
